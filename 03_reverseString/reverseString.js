494b235b25284ce2332b5883c3a7870457bb4555
Ric <74797847+rickatnite@users.noreply.github.com>
2024-09-05 14:42:19 -0400

    Completed exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let str = "";
+    let arr = [];
+    for (let i = 0; i < string.length; i++) {
+        arr.push(string[i]);
+    }
+    let arr2 = arr.reverse().join("");
+    return arr2.toString();
 
 };
 
