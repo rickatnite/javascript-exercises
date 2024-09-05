97449e9848420e5c2de1ddab0cb3f62637e20a0b
Ric <74797847+rickatnite@users.noreply.github.com>
2024-09-05 16:25:52 -0400

    Completed exercise 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theRest) {
+    let arr = []; //new empty array
+    array.forEach((item) => { //iterate using forEach
+        if (!theRest.includes(item)) { //exclude items that match theRest
+            arr.push(item); //push remaining items to arr
+        }
+    });
+    return arr;
 };
 
 // Do not edit below this line
