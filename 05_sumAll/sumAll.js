661ad3d189c4b260513feba7951bdc9884e2df36
Ric <74797847+rickatnite@users.noreply.github.com>
2024-09-06 14:07:09 -0400

    Completed exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+
+    if (!Number.isInteger(min) || !Number.isInteger(max))
+        return "ERROR"; //if min/max are not int throw error
+    if (min < 0 || max < 0) 
+        return "ERROR"; //if min/max are not positive throw error
+    if (min > max) { //swap the max and min with the num
+        let num = min;
+        min = max;
+        max = num;
+    } 
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) { //i must be <= to max
+        sum += i; //increment and add to the sum each loop
+    }
+    return sum;
 
 };
 
