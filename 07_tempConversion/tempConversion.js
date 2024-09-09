369792f381a4b0abcc3f6ba3781bf0db0cdebf13
Ric <74797847+rickatnite@users.noreply.github.com>
2024-09-09 15:07:06 -0400

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let far = temp;
+  let cel = (far - 32) * (5/9);
+  let celsius = Math.round(cel * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let cel = temp;
+  let far = (cel * (9/5) + 32);
+  let fahrenheit = Math.round(far * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
