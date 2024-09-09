52385bddb6a3e7b410e6e6ddb0fd1c76cf0dc14e
Ric <74797847+rickatnite@users.noreply.github.com>
2024-09-09 14:51:10 -0400

    Completed exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let divFour = year % 4 === 0;
+    let divCentury = year % 100 === 0;
+    let divFourHundred = year % 400 === 0;
+
+    if (divFour && !divCentury || divFourHundred) {
+        return true;
+    } else {return false;}
 
 };
 
